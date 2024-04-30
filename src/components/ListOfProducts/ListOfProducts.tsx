0e4c527e990afd109bf42cfc8c0faef856c67cf5
Nata13pr <Nata13pr@gmail.com>
2024-04-30 23:48:53 +0300

    feat:made product

--- src/components/ListOfProducts/ListOfProducts.tsx
+++ src/components/ListOfProducts/ListOfProducts.tsx
@@ -2,12 +2,16 @@ import React, {FC} from 'react';
 import IProduct from "../../Model/IProduct";
 import Product from "../Product/Product";
 
+interface IProps {
+    products: IProduct[]
+}
+
 type ListOfProductsWithChildren<T> = T & { children?: React.ReactNode };
 
-const ListOfProducts: FC<ListOfProductsWithChildren <IProduct[]> > = ({products}) => {
+const ListOfProducts: FC<ListOfProductsWithChildren<IProps>> = ({products}) => {
     return (
         <ul>
-            {products.map(product=><Product key={product.id} product={product}/>)}
+            {products.map(product => <Product key={product.id} product={product}/>)}
         </ul>
     );
 };
--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -8,11 +8,18 @@ interface IProps{
 type ProductComponentWithChildren<T> =T & {children?: ReactNode}
 
 const Product: FC<ProductComponentWithChildren<IProps> >= ({ product}) => {
-
+const {title,rating,brand,thumbnail,description,category,price,images,stock,discountPercentage,}=product;
     return (
         <li>
-            <h1>{product.title}</h1>
-            <p>{product.description}</p>
+            <h1>Title - {title}</h1>
+            <p>D - description{description}</p>
+            <p>Discount Percentage - {discountPercentage}</p>
+            <p>Price - {price}</p>
+            <p>Rating - {rating}</p>
+            <p>Brand - {brand}</p>
+            <p>Stock - {stock}</p>
+            <img src={thumbnail} alt={thumbnail}/>
+            <ListOfImage/>
         </li>
     );
 };
