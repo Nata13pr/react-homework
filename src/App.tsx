3f6232131ab102030d324ec05d3f8c3f0b0fa4a5
nata13pr <Nata13pr@gmail.com>
2024-06-16 13:59:33 +0300

    feat:used Navigate

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {useDispatch} from "react-redux";
+import {useAppDispatch} from "./redux/store";
+import {userActions} from "./redux/slices/userSlice";
+import {postActions} from "./redux/slices/postSlice";
+import HeaderComponent from "./components/HeaderComponent";
+import {Outlet} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        dispatch(userActions.loadUsers());
+        dispatch(postActions.loadPosts())
+    }, []);
+
+    return (
+        <div>
+            <HeaderComponent/>
+            <Outlet/>
+        </div>
+    );
 }
 
 export default App;
